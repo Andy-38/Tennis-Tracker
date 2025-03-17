21b9183ece04b63d2c26e515614f9bd971297602
Andrey Dvoytsov <annihilator1981@mail.ru>
2025-03-17 21:26:34 +0500

    Done button

--- Tennis Tracker/ViewController.swift
+++ Tennis Tracker/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    
+    @IBOutlet weak var TurnirTextField: UITextField! // поле ввода названия турнира
+    
+    @IBOutlet weak var FirstPlayerNameTextField: UITextField! // поле ввода имени 1-го игрока
+    
+    @IBOutlet weak var SecondPlayerNameTextField: UITextField! // поле ввода имени 2-го игрока
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // TurnirTextField.delegate = self
     }
-
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // вызывается когда пользователь коснулся экрана
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true) // убираем клавиатуру
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // вызывается при нажатии кнопки "готово"
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
